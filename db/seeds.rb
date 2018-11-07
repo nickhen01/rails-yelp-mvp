1c89b6f9881c8bb311aa38f6572eb5fbe8ab3c29
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-07 12:25:52 +0000

    created seed restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0789789789',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0555666888',
+    category:     'italian'
+  },
+  {
+    name:         'Flat Iron',
+    address:      'Soho, London W1',
+    phone_number:  '0222333444',
+    category:     'french'
+  },
+  {
+    name:         'Zedel',
+    address:      'Soho, London W1',
+    phone_number:  '09988776655',
+    category:     'french'
+  },
+  {
+    name:         'McDonald',
+    address:      'Victoria, London W1',
+    phone_number:  '01234567891',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
