86b5a085527330993d1d745a6e0b792d842e94dd
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-07 15:03:28 +0000

    add action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,12 +4,21 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
